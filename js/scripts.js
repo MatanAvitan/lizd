1be1ac9ecccb3e5bf34c79ef2e93dfb868dd1cb3
ma <matan13av@gmail.com>
2017-07-21 03:16:00 +0300

    first upload

--- js/scripts.js
+++ js/scripts.js
@@ -110,7 +110,7 @@
 		
         $(window).load(function(){
         $(".typing").typed({
-            strings: ["I am A Freelancer.", "I am Execution Engineer.", "We should create big things TOGTHER."],    /* You can change the home section typing text from
+            strings: ["I am a Freelancer.", "I Design and stylish flowers for events.", "We should create big things TOGTHER."],    /* You can change the home section typing text from
 	                                                                                            here and do not use "&" use "and" */
             typeSpeed: 50
           });
